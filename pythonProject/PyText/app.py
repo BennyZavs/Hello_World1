1d365ea95c94644e9c078da9e80e2283281378e4
Benjamin Zavulunov <108142009+BennyZavs@users.noreply.github.com>
2022-12-26 14:44:58 -0500

    Update app.py

--- pythonProject/PyText/app.py
+++ pythonProject/PyText/app.py
@@ -3,13 +3,13 @@ from http import client
 from pydoc import cli
 from twilio.rest import Client
 
-account_sid = "ACd6f696870ade2c4d9d4e927213539754"
-auth_token = "5c869801e8dc67ad7e15d1c80ad51bd3"
+account_sid = "_Insert Acct SID_"
+auth_token = "_Insert Auth Token_"
 client = Client(account_sid, auth_token)
 
 call = client.messages.create(
-    to='4706097040',
-    from_='8596511817',
+    to='_Insert Phone Number_',
+    from_='_Insert Phone Number_',
     body="This messsage was sent out using Ben's code in python, wohoo!"
 )
 
